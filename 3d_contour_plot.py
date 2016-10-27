3bc254b8a34d73fa403e29edc14cdefee680459f
Josh Levy-Kramer <joshlk@users.noreply.github.com>
2016-10-27 11:59:03 +0100

    Create 3d_contour_plot.py

---
+++ 3d_contour_plot.py
@@ -0,0 +1,26 @@
+def conout_plot(x, y, z, nx=100, ny=100, interp='linear'):
+    """
+    Note grid interpolation can take a long time
+
+    :param x:
+    :param y:
+    :param z:
+    :param nx: How many x intervals in the z grid
+    :param ny: How many y intervals in the z grid
+    :param interp: Interpolation algorithm, either 'nn' for natural neighbor, or 'linear' for linear interpolation
+    :return:
+    """
+
+    x_min, x_max = x.max(), x.min()
+    y_min, y_max = y.max(), y.min()
+
+    xi = np.linspace(x_min, x_max, nx)  # x grid intervals
+    yi = np.linspace(y_min, y_max, ny)  # y grid intervals
+    zi = ml.griddata(x, y, z, xi, yi, interp=interp)   # z grid
+
+    plt.contour(xi, yi, zi, 15, linewidths=0.5, colors='k')
+    plt.pcolormesh(xi, yi, zi, cmap=plt.get_cmap('rainbow'))
+
+    plt.colorbar()
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
